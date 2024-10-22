67fae93d2501ff817b87b89139d200f798f281ae
dingopes <olmyta@gmail.com>
2024-10-22 19:56:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  base: '/your-repo-name/', // Replace 'your-repo-name' with your actual repository name
-  plugins: [svelte()]
+  plugins: [svelte()],
+  build: {
+    outDir: 'build'
+  }
 });
